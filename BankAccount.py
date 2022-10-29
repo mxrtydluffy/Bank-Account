67f011593aeaa5619c9708f49b8a4baa77cb4865
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-10-28 21:25:33 -0700

    added def deposit/withdraw

--- BankAccount.py
+++ BankAccount.py
@@ -3,9 +3,27 @@ import random
 #Defined BankAccount
 #random.randint gets random integer
 class BankAccount:
-    def __init__(self, fullname):
+    def __init__(self, fullname, acount_number, balance):
         self.name = fullname
         self.account_number = random.randint(91467395)
         self.balance = 0
 
 #Define methods for BankAccount
+    def deposit(self, amount):
+        """
+        Function adds inputed amount to account balance.
+        Displays a message for a new updated balance.
+        """
+        self.balance = self.balance + amount
+        return self.balance
+    
+    def withdraw(self, amount):
+        """
+        Function substracts a specificed amount from the user's balance.
+        Displays a message for the amount wiuthdrawn.
+        """
+        self.balance = self.balance - amount
+        if self.balance < 0:    # For situations only the user knows without explicitly stating exactly. 
+            print('Error please try again')
+        else:
+            return self.balance
